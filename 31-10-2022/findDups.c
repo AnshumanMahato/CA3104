a7e920dc2a33148fd42a07d5ddbce3f2b66af805
AnshumanMahato <58422570+AnshumanMahato@users.noreply.github.com>
2022-11-06 18:25:47 +0530

    modifications

--- 31-10-2022/findDups.c
+++ 31-10-2022/findDups.c
@@ -1,38 +1,31 @@
 #include <stdio.h>
 
-int main()
-{
-    int size;
-    printf("Enter the size of square the matrix: ");
-    scanf("%d", &size);
-    int a[size * size];
-    printf("\nEnter %d numbers:", size * size);
-    for (int i = 0; i < size * size; i++)
-    {
+int main(){
+    int row,col;
+    printf("Enter the no. of rows and columns for the matrices: ");
+    scanf("%d%d",&row,&col);
+    int a[row * col];
+    printf("\nEnter %d numbers:", row * col);
+    for (int i = 0; i < row * col; i++){
         scanf("%d", &a[i]);
     }
 
     printf("\nMatrix is\n");
-    for (int i = 0; i < size * size; i++)
-    {
-        if (i % size == 0)
+    for (int i = 0; i < row * col; i++){
+        if (i % col == 0)
             printf("\n");
         printf("%d ", a[i]);
     }
 
     printf("\nDuplicate positions:\n");
-    for (int i = size * size - 1; i >= 0; i--)
-    {
-        if (a[i] != 0)
-        {
+    for (int i = row * col - 1; i >= 0; i--){
+        if (a[i] != 0){
             printf("%d : ", a[i]);
             int dup = 0;
-            for (int j = i - 1; j >= 0; j--)
-            {
-                if (a[i] == a[j])
-                {
+            for (int j = i - 1; j >= 0; j--){
+                if (a[i] == a[j]){
                     a[j] = 0;
-                    printf("(%d,%d) ", j / size, j % size);
+                    printf("(%d,%d) ", j / col, j % col);
                     dup = 1;
                 }
             }
@@ -42,10 +35,9 @@ int main()
         }
     }
 
-    printf("\nMatrix is\n");
-    for (int i = 0; i < size * size; i++)
-    {
-        if (i % size == 0)
+    printf("\nUnique Matrix is\n");
+    for (int i = 0; i < row * col; i++){
+        if (i % col == 0)
             printf("\n");
         if (a[i])
             printf("%d ", a[i]);
