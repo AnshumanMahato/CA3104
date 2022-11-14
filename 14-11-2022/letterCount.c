2f0cf90126f7ea3f4a3a05318b83da11a5a748fe
Anshuman Mahato <58422570+AnshumanMahato@users.noreply.github.com>
2022-11-14 15:51:04 +0530

    Create letterCount.c

---
+++ 14-11-2022/letterCount.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<conio.h>
+#include<ctype.h>
+
+int isvowel(char ch){
+	if(ch>='A' && ch<='Z')
+		ch+=32;
+	if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+		return 1;
+	else
+		return 0;
+}
+
+
+int main(){
+	char str[1000];
+	int i=0,letters=0,vowels=0,words=0;
+	printf("Enter Sentence:");
+	gets(str);
+	while(str[i] != '\0'){
+		if(isalpha(str[i])){
+			letters++;
+			if(isvowel(str[i]))
+				vowels++;
+		}
+		else if(str[i] == ' ')
+			words++;
+		i++;
+	}
+	
+	printf("letters:%d\nvowels: %d\nwords: %d",letters,vowels,words+1);
+	return 0;
+}
