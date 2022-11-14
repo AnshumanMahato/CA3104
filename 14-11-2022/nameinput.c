d73b7bb71c9e4be65641a9b569414413cdb4ba34
Anshuman Mahato <58422570+AnshumanMahato@users.noreply.github.com>
2022-11-14 15:50:38 +0530

    Create nameinput.c

---
+++ 14-11-2022/nameinput.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+
+int main(){
+	char name1[100],name2[100],name3[100];
+	int i =0;
+	printf("Enter your name1:\n");
+	while(1){
+		scanf("%c",&name1[i]);
+		if(name1[i]=='\n')
+		{
+			name1[i] = '\0';
+			break;
+		}
+		i++;
+	}
+	printf("Enter your name2:\n");
+	scanf("%s%s",name2,name3);
+	strcat(name2," ");
+	strcat(name2,name3);
+	fflush(stdin);
+	printf("Enter your name3:\n");
+	gets(name3);
+	
+	
+	printf("\nName1: %s",name1);
+	printf("\nName2: %s",name2);
+	printf("\nName3: %s",name3);
+	return 0;
+}
