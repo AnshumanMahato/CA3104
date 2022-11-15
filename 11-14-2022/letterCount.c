f2a2643539ab5c833bfe39bdc684d66f39cbdfa5
AnshumanMahato <58422570+AnshumanMahato@users.noreply.github.com>
2022-11-15 20:00:03 +0530

    compiled

--- 11-14-2022/letterCount.c
+++ 11-14-2022/letterCount.c
@@ -1,33 +1,36 @@
-#include<stdio.h>
-#include<conio.h>
-#include<ctype.h>
+#include <stdio.h>
+#include <conio.h>
+#include <ctype.h>
 
-int isvowel(char ch){
-	if(ch>='A' && ch<='Z')
-		ch+=32;
-	if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+int isvowel(char ch)
+{
+	if (ch >= 'A' && ch <= 'Z')
+		ch += 32;
+	if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
 		return 1;
 	else
 		return 0;
 }
 
-
-int main(){
+int main()
+{
 	char str[1000];
-	int i=0,letters=0,vowels=0,words=0;
-	printf("Enter Sentence:");
+	int i = 0, letters = 0, vowels = 0, words = 0;
+	printf("Enter Sentence:\n");
 	gets(str);
-	while(str[i] != '\0'){
-		if(isalpha(str[i])){
+	while (str[i] != '\0')
+	{
+		if (isalpha(str[i]))
+		{
 			letters++;
-			if(isvowel(str[i]))
+			if (isvowel(str[i]))
 				vowels++;
 		}
-		else if(str[i] == ' ')
+		else if (str[i] == ' ')
 			words++;
 		i++;
 	}
-	
-	printf("letters:%d\nvowels: %d\nwords: %d",letters,vowels,words+1);
+
+	printf("letters:%d\nvowels: %d\nconsonants: %d\nwords: %d", letters, vowels, letters - vowels, words + 1);
 	return 0;
 }
